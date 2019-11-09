545e3b2442c5f479a81d3e939049d33193172278
Steven Boada <stevenboada@gmail.com>
2019-11-09 12:46:33 -0500

    chap. 2

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,20 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
     user = {'username': 'Steven'}
-
-    return f'''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, {user['username']}! </h1>
-    </body>
-</html>'''
-
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
