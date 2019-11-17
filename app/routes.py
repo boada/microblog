84f2d1a71327da3f6283b74a3b3d722e034b2f5f
Steven Boada <stevenboada@gmail.com>
2019-11-16 19:57:33 -0500

    chapter 3

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'username': 'Steven'}
     posts = [
@@ -18,3 +18,12 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, '
+              f'remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title='Sign In', form=form)
